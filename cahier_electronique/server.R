e062b3b62b16dce64f2af03362be6c26edf8468b
Marie <marie.jeremie@outlook.fr>
2018-06-14 12:56:52 +0200

    Ajout de la concatenation des champs d'UNE blessure

--- cahier_electronique/server.R
+++ cahier_electronique/server.R
@@ -299,14 +299,14 @@ listAnimal = dbGetQuery(con,"select distinct ani_etiq from public.t_animal_ani")
       for (u in input$traitement) {
         list_ble = paste(u,list_ble,sep=" ")
       }
-      blessure <<- rbind(blessure,data.frame("Localisation" = c(input$locali), "Gravite" =c(input$grave), "Traitement" = c(list_ble)))
+      blessure <<- rbind(blessure,data.frame("Localisation" = c(input$locali), "Gravite" =c(input$grave), "Traitement" = c(list_ble), "Liste" = paste0(c(input$locali)," - ",c(input$grave), " - ",c(list_ble))))
       updateSelectizeInput(session,"locali", options=list(selected=NULL))
       updateSelectizeInput(session,"traitement", options=list(selected=NULL))
     }
       
     if ((length(input$traitement))==1)
       {
-        blessure <<- rbind(blessure,data.frame("Localisation" = c(input$locali), "Gravite" =c(input$grave), "Traitement" = c(input$traitement)))
+        blessure <<- rbind(blessure,data.frame("Localisation" = c(input$locali), "Gravite" =c(input$grave), "Traitement" = c(input$traitement), "Liste" = paste0(c(input$locali)," - ",c(input$grave), " - ",c(input$traitement))))
     }
     
     output$tableblessure = DT::renderDT(blessure,server = F)
